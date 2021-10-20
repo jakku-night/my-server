22fed0b4c0cc36e7038fbbc8c519340555659618
Jakku Night <J97k107u45k117n@gmail.com>
2021-10-20 21:25:08 +0000

    Setting up Heroku deploying

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const { promisify } = require('util');
 
 const db = mysql.createPool({
-    host: process.env.JAWSDB_MARIA_URL,
+    host: 'localhost',
     user: 'user',
     password: 'password',
     database: 'db',
